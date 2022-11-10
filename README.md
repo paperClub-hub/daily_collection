791355f91a0e244d7b24df99632f8b41eb05a945
paperClub-hub <qhs2018@sina.com>
2022-11-10 14:36:39 +0800

    init

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 #### 2. py/docment_text2sentences.py, 将文本（中文）按照标点符号拆分成单句，并保留原始标点符号
 #### 3. py/rule_pattern_demo1.py, 基于规则处理 “室内面积”、“费用”等实体关键字
 #### 4. py/zipfiles.py, 基于协程进行文件压缩，压缩后自动删除原始文件
+#### 5. py/logging_with_different_color.py, 根据日志登记显示不同的颜色
---
+++ py/logging_with_different_color.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Projecte : PyCharm
+# @Date     : 2022-11-10 13:35
+# @Author   : NING MEI
+# @Desc     :
+
+
+import os
+import logging
+import colorlog
+import functools
+from functools import partial
+
+log_config = {
+    'DEBUG': {
+        'level': 10,
+        'color': 'purple'
+    },
+    'INFO': {
+        'level': 20,
+        'color': 'green'
+    },
+    'TRAIN': {
+        'level': 21,
+        'color': 'cyan'
+    },
+    'EVAL': {
+        'level': 22,
+        'color': 'blue'
+    },
+    'WARNING': {
+        'level': 30,
+        'color': 'yellow'
+    },
+    'ERROR': {
+        'level': 40,
+        'color': 'red'
+    },
+    'CRITICAL': {
+        'level': 50,
+        'color': 'bold_red'
+    }
+}
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logger = logging.getLogger(__name__)
+
+class Logger(object):
+    '''
+    Deafult logger in UIE
+
+    Args:
+        name(str) : Logger name, default is 'UIE'
+    '''
+
+    def __init__(self, name: str = None):
+        name = 'UIE' if not name else name
+        self.logger = logging.getLogger(name)
+
+        for key, conf in log_config.items():
+            logging.addLevelName(conf['level'], key)
+            self.__dict__[key] = functools.partial(
+                self.__call__, conf['level'])
+            self.__dict__[key.lower()] = functools.partial(
+                self.__call__, conf['level'])
+
+        self.format = colorlog.ColoredFormatter(
+            '%(log_color)s[%(asctime)-15s] [%(levelname)8s]%(reset)s - %(message)s',
+            log_colors={key: conf['color']
+                        for key, conf in log_config.items()})
+
+        self.handler = logging.StreamHandler()
+        self.handler.setFormatter(self.format)
+
+        self.logger.addHandler(self.handler)
+        self.logLevel = 'DEBUG'
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.propagate = False
+        self._is_enable = True
+
+    def disable(self):
+        self._is_enable = False
+
+    def enable(self):
+        self._is_enable = True
+
+    @property
+    def is_enable(self) -> bool:
+        return self._is_enable
+
+    def __call__(self, log_level: str, msg: str):
+        if not self.is_enable:
+            return
+
+        self.logger.log(log_level, msg)
+
+
+
+
+
+logger = Logger()
+
+if __name__ == '__main__':
+    logger.info("hello ")
+    logger.warning(' haha ')
+    logger.error(' no ')
