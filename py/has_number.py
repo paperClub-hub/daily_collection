45459e07f29489b32dcb7af3f975587304d7cdb5
paperClub-hub <qhs2018@sina.com>
2022-11-16 18:20:29 +0800

    init

---
+++ py/has_number.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Projecte : PyCharm
+
+
+import re
+
+
+""" 检查字符串中是否有数值型数据"""
+
+reg = '([\d|一|二|三|四|五|六|七|八|九|十|壹|贰|叁|弎|仨|肆|伍|陆|柒|捌|玖|俩|两|零|百|千|万|亿|兆|拾|佰|仟|萬|億]+)'
+reg += "|(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?Dec(ember)?)"
+reg +="|(zero|one|two|three|four|five|six|seven|eight|nine)"
+
+patten = re.compile(reg)
+
+
+text = ["今天星期三", "电话119", "one boy"]
+
+for x in text:
+	state = patten.search(x)
+	if state:
+		print(f"{x}: =>  {state.group()}")
+
