724b383e49075c81f773332f849128ccaa192847
paperClub-hub <qhs2018@sina.com>
2022-11-25 11:15:28 +0800

    更新

--- py/has_number.py
+++ py/has_number.py
@@ -8,19 +8,30 @@
 import re
 
 
-""" 检查字符串中是否有数值型数据"""
+def is_numerical(word:str):
+	""" 检查字符串中是否有数值型数据"""
 
-reg = '([\d|一|二|三|四|五|六|七|八|九|十|壹|贰|叁|弎|仨|肆|伍|陆|柒|捌|玖|俩|两|零|百|千|万|亿|兆|拾|佰|仟|萬|億]+)'
-reg += "|(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?Dec(ember)?)"
-reg +="|(zero|one|two|three|four|five|six|seven|eight|nine)"
+	reg = '([\d|一|二|三|四|五|六|七|八|九|十|壹|贰|叁|弎|仨|肆|伍|陆|柒|捌|玖|俩|两|零|百|千|万|亿|兆|拾|佰|仟|萬|億]+)'
+	reg += "|(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?Dec(ember)?)"
+	reg +="|(zero|one|two|three|four|five|six|seven|eight|nine)"
 
-patten = re.compile(reg)
+	state = False
+	vlaue = ""
+	patten = re.compile(reg)
+	res = patten.search(word)
+	if res:
+		state = True
+		vlaue = res.group()
+
+	return state, vlaue
+
+
 
 
 text = ["今天星期三", "电话119", "one boy"]
 
 for x in text:
-	state = patten.search(x)
+	state, value = is_numerical(x)
 	if state:
-		print(f"{x}: =>  {state.group()}")
+		print(f"{x}: =>  is_numerical: {state}, value: {value}")
 
